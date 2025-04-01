db5741467b4a42a1ad5cd5fb4edbb34104997f9e
voidzeronull0 <user@omen.localdomain>
2025-04-01 11:07:59 +0300

    Add simple protobuf support and UT

--- dasboot/controller/controller.cpp
+++ dasboot/controller/controller.cpp
@@ -2,7 +2,51 @@
 
 namespace NController {
 
-    bool TController::Build(const TBuildSettings& buildSettings) {
-        return false;
+    TReferencingRule::TReferencingRule(const string& reference, const bool isReferencingById) {
+        Reference = reference;
+        IsReferencingById = isReferencingById;
+    }
+    TReferencingRule::~TReferencingRule() {};
+
+    TStartSettings::TStartSettings(const TReferencingRule& container) {
+        Container = container;
+    }
+
+    TStopSettings::TStopSettings(const TReferencingRule&  container) {
+        Container = container;
+    }
+
+    TRemoveSettings::TRemoveSettings(const TReferencingRule& container) {
+        Container = container;
+    }
+
+    TExecuteSettings::TExecuteSettings(const TReferencingRule& container, 
+    const string command, const string args) {
+        Container = container;
+        Command = command;
+        Args = args;
+    }
+
+    TGlobalConfig::TGlobalConfig() {}
+    TGlobalConfig::~TGlobalConfig() {}
+
+    TController::TController() {
+        globalConfig = TGlobalConfig();
+    }
+    TController::~TController() {};
+
+    TRequest TController::Build(const TBuildSettings& buildSettings) {
+        TRequest request;
+        request.set_type("build");
+
+        TArg* arg1 = request.add_args();
+        arg1->set_name("PathToDasbootFile");
+        arg1->set_value(buildSettings.PathToDasbootFile);
+
+        TArg* arg2 = request.add_args();
+        arg2->set_name("ContainerName");
+        arg2->set_value(buildSettings.ContainerName.value_or(std::to_string(rand())));
+
+        return request;
     }
 }
--- dasboot/controller/controller_ut.cpp
+++ dasboot/controller/controller_ut.cpp
@@ -2,10 +2,28 @@
 
 #include <dasboot/controller/controller.hpp>
 
-TEST(ControllerUt, BuildContainer) {
+TEST(ControllerUt, BuildContainerRequest) {
     NController::TController controller;
     NController::TBuildSettings buildSettings;
-    ASSERT_EQ(controller.Build(buildSettings), false);
+    buildSettings.PathToDasbootFile = "path/to/dasbootfile";
+    buildSettings.ContainerName = "Test";
+
+    NController::TRequest expectedRequest;
+    expectedRequest.set_type("build");
+    NController::TArg* arg1 = expectedRequest.add_args();
+    arg1->set_name("PathToDasbootFile");
+    arg1->set_value(buildSettings.PathToDasbootFile);
+
+    NController::TArg* arg2 = expectedRequest.add_args();
+    arg2->set_name("ContainerName");
+    arg2->set_value(buildSettings.ContainerName.value());
+
+    NController::TRequest resultRequest = controller.Build(buildSettings);
+    EXPECT_EQ(expectedRequest.type(), resultRequest.type());
+    EXPECT_EQ(expectedRequest.args(0).name(), resultRequest.args(0).name());
+    EXPECT_EQ(expectedRequest.args(0).value(), resultRequest.args(0).value());
+    EXPECT_EQ(expectedRequest.args(1).name(), resultRequest.args(1).name());
+    EXPECT_EQ(expectedRequest.args(1).value(), resultRequest.args(1).value());
 }
 
 int main(int argc, char** argv) {
