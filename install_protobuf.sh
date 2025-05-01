03abbe7bc82ea615206ec0619c8d55b50ebbbd3f
Igor <igorkoshkarev4qqq@mail.ru>
2025-05-01 13:27:06 +0300

    micro fixes

--- install_protobuf.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/protocolbuffers/protobuf && \
-cd protobuf && \
-cmake . && \
-cmake --build . --parallel 8 && \
-sudo cmake --install . && \
-cd .. && \
-rm -rf protobuf
