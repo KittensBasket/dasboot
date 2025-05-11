32e67e30bc886cbcef2a26848c2d805ca071f7e4
Igor <igorkoshkarev4qqq@mail.ru>
2025-05-11 18:37:30 +0300

    add dummy ut

--- dasboot/core/container/container_ut.cpp
+++ dasboot/core/container/container_ut.cpp
@@ -5,6 +5,10 @@
 
 #include <dasboot/core/container/container.hpp>
 
+TEST(Containers, DummyTest) {
+    EXPECT_EQ(true, true);
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     auto result = RUN_ALL_TESTS();
